62d48ef55ee3eb24ef4bf9ac7b519d8cd788c25e
goriunovd <gotiunovd@gmail.com>
2016-11-19 20:08:04 +1300

    Initial push , with first page

---
+++ Redmond/ViewModels/initialPage.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Redmond
+{
+	public class initialPage : ContentPage
+	{
+
+		Button navigationButton;
+		public void ComonStyles (){
+			BackgroundColor = Color.FromHex("#1259CD");
+			NavigationPage.SetHasNavigationBar(this, false);
+		}
+		public initialPage()
+		{
+			Device.OnPlatform(
+				iOS:()=>{
+				ComonStyles();
+				Padding = new Thickness(10,20,10,10);
+				},
+				Android: () =>{
+				ComonStyles();
+				Padding = new Thickness(10, 0 , 10 ,10);
+				},
+				Default: () =>{
+				ComonStyles();
+			 	}
+			);
+
+			navigationButton = new Button{
+				Text = "Start ordering",
+
+				FontSize = 20,
+				TextColor = Color.White,
+				BackgroundColor = Color.FromHex("#3E7EE7")
+			};
+
+			StackLayout mainContent = new StackLayout
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.Center,
+				Padding = new Thickness(0 , 60 ,0 ,0),
+				Children = {
+					navigationButton
+				}
+
+			};
+
+			Content = new StackLayout
+			{
+				Padding = new Thickness(0 , 100 ,0 ,0),
+				Spacing = 40,
+				Children = {
+					new Label{
+						HorizontalTextAlignment = TextAlignment.Center,
+						FontSize = 35,
+						Text = "Redmond",
+						TextColor = Color.White,
+						FontAttributes = FontAttributes.Bold
+					},
+					new Label{
+						HorizontalTextAlignment = TextAlignment.Center,
+						FontSize= 15,
+						Text = "Order your faivorite food from your mobile",
+						TextColor = Color.White,
+						FontAttributes = FontAttributes.Italic
+					},
+					mainContent
+				}
+			};
+		}
+	}
+}
+
